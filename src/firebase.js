141e40777f599ce2d301e598354571dae4f23621
Saheed Askar <gmcrmicro@gmail.com>
2026-05-15 17:00:16 +0530

    Fixed Firebase API key

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
 
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBvL_evH7aioAaBYYHkx2SsguTEdHWbx_Q",
+
+  apiKey: "AIzaSyBvl_evhZaioAaBYYHkx2SsguTEdHWbx_Q",
+
   authDomain: "infection-control-ai.firebaseapp.com",
+
   projectId: "infection-control-ai",
+
   storageBucket: "infection-control-ai.firebasestorage.app",
+
   messagingSenderId: "844489040257",
+
   appId: "1:844489040257:web:1fa3b804d5eae453860880",
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
